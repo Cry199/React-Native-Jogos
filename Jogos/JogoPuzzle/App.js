a8bece54225f5f8ecd63ead4e51af4850a2a7f4e
Cauã <caualeu2016@gmail.com>
2023-10-07 06:39:01 -0300

    Jogo Puzzle - v0.0.1

---
+++ Jogos/JogoPuzzle/App.js
@@ -0,0 +1,14 @@
+import {Text, View } from 'react-native';
+
+import { styles } from './styles/stylesGeral.js';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <Text>Open up App.js to start working on your app!</Text>
+      
+    </View>
+  );
+}
+
