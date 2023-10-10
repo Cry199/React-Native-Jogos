baa6fb81ed2bdc1181fe0cc5cb4fb79cc8d1c4c0
Cauã <caualeu2016@gmail.com>
2023-10-10 16:52:53 -0300

    Jogo Flappy Bird - v0.7.5 - Animated Bird

--- Jogos/JogoFlappyBird/components/Bird.js
+++ Jogos/JogoFlappyBird/components/Bird.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
-import { View, Image } from "react-native";
+import { View, Image, Animated } from "react-native";
 import Images from '../assets/Images.js';
+
 
 export default class Bird extends Component 
 {
+    constructor(props){
+        super(props);
+
+        this.animatedValue = new Animated.Value(this.props.body.velocity.y); 
+    }
+
     render()
     {
         const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
         const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
         const x = this.props.body.position.x - width / 2;
         const y = this.props.body.position.y - height / 2;
+
+        this.animatedValue.setValue(this.props.body.velocity.y);
+        let rotation = this.animatedValue.interpolate({
+            inputRange: [-10, 0, 10, 20],
+            outputRange: ['-20deg', '0deg', '15deg', '45deg'],
+            extrapolate: 'clamp',
+        });
 
         let image = Images['bird' + this.props.pose];
 
@@ -20,6 +34,7 @@ export default class Bird extends Component
                 top: y,
                 width: width,
                 height: height,
+                transform: [{ rotate: JSON.stringify(rotation).replace(/"/g, "") }]
             }} 
             resizeMode="stretch"
             source={image}/>
