f0b61a03bf00568025bb0390ae5471472357d077
Cauã <caualeu2016@gmail.com>
2023-10-10 02:39:14 -0300

    Jogo Flappy Bird - v0.0.2 - Bird Na Tela

--- Jogos/JogoFlappyBird/App.js
+++ Jogos/JogoFlappyBird/App.js
@@ -1,23 +1,49 @@
-import { Text, View, Alert, TouchableOpacity } from 'react-native';
 import React from 'react';
+import Matter from 'matter-js';
+
+import { Text, View, Alert, TouchableOpacity } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
 
 import { styles } from './assets/styles/stylesGeral.js';
-
 import Constants from './components/Constants/Constants.js';
+import Bird from './components/Bird.js';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
   }
 
+  setupWorld = () => 
+  {
+    let engine = Matter.Engine.create({ enableSleeping: false });
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, 
+    Constants.MAX_HEIGHT / 2, 50, 50);
+
+    Matter.World.add(world, [bird]);
+
+    return {
+      physics: { engine: engine, world: world },
+      bird: { body: bird, size: [50,50], color: 'red' ,renderer: Bird },
+    }
+
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start aasdaour app!</Text>
+        <GameEngine ref={(ref) => { this.gameEngine = ref; }} style={styles.baseDoJogo}
+         entities={this.entities} >
+            
+        </GameEngine>
       </View>
     );
   }
 }
+
 
 
