a00bd30953971b879c3e76f1d3ab169e7c50cc11
Cauã <caualeu2016@gmail.com>
2023-10-14 09:38:00 -0300

    Jogo Da Cobrinha - v0.0.1

--- Jogos/JogoDaCobrinha/App.js
+++ Jogos/JogoDaCobrinha/App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { GameEngine } from "react-native-game-engine"
 
-export default function App() {
+import { styles } from './assets/styles/stylesGeral.js';
+
+export default function App() 
+{
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Começando</Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
